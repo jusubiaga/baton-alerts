2ce3a14411e97d970c98188da584ddb6738a8162
Juan Usubiaga <juan.usubiaga@bitlogic.io>
2025-03-27 18:16:38 -0300

    fix: scroll in table catalogo

--- app/(protected)/automations/_components/BotCatalogTable.tsx
+++ app/(protected)/automations/_components/BotCatalogTable.tsx
@@ -10,21 +10,19 @@ import {
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
-  getPaginationRowModel,
   getSortedRowModel,
   useReactTable,
 } from "@tanstack/react-table";
 
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { createBotAction, getBotAction } from "@/actions/bot";
-import { addRuleToCatalog } from "@/data/catalogs";
+import { createBotAction } from "@/actions/bot";
 import { toast } from "sonner";
 import { getRulesAction } from "@/actions/rules";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useRouter } from "next/navigation";
 import { Loader2 } from "lucide-react";
-import { Badge } from "@/components/ui/badge";
+import { ScrollArea } from "@/components/ui/scroll-area";
 
 enum InstaledStutus {
   "INSTALL" = "Install",
@@ -89,61 +87,6 @@ const ActionButton = ({ row, onUpdate }: { row: any; onUpdate: () => void }) => 
   );
 };
 
-// export const columns: ColumnDef<any>[] = [
-//   {
-//     accessorKey: "id",
-//     header: "Bot ID",
-//     cell: ({ row }) => (
-//       <div className="flex items-center gap-2">
-//         <div className="w-[40px] h-[40px]">
-//           <Avatar>
-//             <AvatarImage src={row.original.avatar} alt="@cald" />
-//             <AvatarFallback>I</AvatarFallback>
-//           </Avatar>
-//         </div>
-//         {row.getValue("id")}
-//       </div>
-//     ),
-//   },
-//   {
-//     accessorKey: "name",
-//     header: ({ column }) => {
-//       return (
-//         <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-//           Bot Name
-//           <ArrowUpDown className="ml-2 h-4 w-4" />
-//         </Button>
-//       );
-//     },
-//     cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
-//   },
-//   {
-//     accessorKey: "available",
-//     header: ({ column }) => {
-//       return (
-//         <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-//           <ArrowUpDown className="ml-2 h-4 w-4" />
-//         </Button>
-//       );
-//     },
-
-//     cell: ({ row }) => <div className="capitalize">{row.getValue("available") ? "Available" : "Coming soon"}</div>,
-//   },
-//   {
-//     accessorKey: "installed",
-//     header: "Installed",
-//     cell: ({ row }) => <div className="capitalize">{row.getValue("installed") ? "Inst" : "Not Inst"}</div>,
-//   },
-
-//   {
-//     accessorKey: "action",
-//     header: "",
-//     cell: ({ row }) => {
-//       return <ActionButton key={row.original.id} row={row} onUpdate={() => rulesData()}></ActionButton>;
-//     },
-//   },
-// ];
-
 type BotCatalogTableProps = {
   search: string;
 };
@@ -185,25 +128,6 @@ export default function BotCatalogTable({ search }: BotCatalogTableProps) {
       },
       cell: ({ row }) => <div className="capitalize">{row.getValue("name")}</div>,
     },
-    // {
-    //   accessorKey: "available",
-    //   header: ({ column }) => {
-    //     return (
-    //       <Button variant="ghost" onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}>
-    //         <ArrowUpDown className="ml-2 h-4 w-4" />
-    //       </Button>
-    //     );
-    //   },
-
-    //   cell: ({ row }) => <Badge variant="outline">{row.getValue("available") ? "Available" : "Coming soon"}</Badge>,
-    //   // <div className="capitalize">{row.getValue("available") ? "Available" : "Coming soon"}</div>,
-    // },
-    // {
-    //   accessorKey: "installed",
-    //   header: "Installed",
-    //   cell: ({ row }) => <div className="capitalize">{row.getValue("installed") ? "Inst" : "Not Inst"}</div>,
-    // },
-
     {
       accessorKey: "action",
       header: "",
@@ -214,9 +138,6 @@ export default function BotCatalogTable({ search }: BotCatalogTableProps) {
   ];
 
   const rulesData = async () => {
-    // console.log("rulesData");
-    // const bots = await getRulesAction();
-    // setData(() => bots as Array<any>);
     setLoading(true); // Activa el estado de carga
     try {
       const bots = await getRulesAction();
@@ -234,7 +155,6 @@ export default function BotCatalogTable({ search }: BotCatalogTableProps) {
     onSortingChange: setSorting,
     onColumnFiltersChange: setColumnFilters,
     getCoreRowModel: getCoreRowModel(),
-    getPaginationRowModel: getPaginationRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
     onColumnVisibilityChange: setColumnVisibility,
@@ -244,12 +164,6 @@ export default function BotCatalogTable({ search }: BotCatalogTableProps) {
       columnFilters,
       columnVisibility,
       rowSelection,
-    },
-    initialState: {
-      pagination: {
-        pageIndex: 0,
-        pageSize: 5, // Limita las filas a 5 por página
-      },
     },
   });
 
@@ -265,66 +179,51 @@ export default function BotCatalogTable({ search }: BotCatalogTableProps) {
     <div className="h-[80%]">
       <div className="w-full">
         <div className="rounded-md border">
-          <Table>
-            <TableHeader>
-              {table.getHeaderGroups().map((headerGroup) => (
-                <TableRow key={headerGroup.id}>
-                  {headerGroup.headers.map((header) => {
-                    return (
-                      <TableHead key={header.id} className="text-center">
-                        {header.isPlaceholder ? null : flexRender(header.column.columnDef.header, header.getContext())}
-                      </TableHead>
-                    );
-                  })}
-                </TableRow>
-              ))}
-            </TableHeader>
-            <TableBody>
-              {table.getRowModel().rows?.length ? (
-                table.getRowModel().rows.map((row) => (
-                  <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
-                    {row.getVisibleCells().map((cell) => (
-                      <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
-                    ))}
+          <ScrollArea className="w-full h-96">
+            <Table>
+              <TableHeader>
+                {table.getHeaderGroups().map((headerGroup) => (
+                  <TableRow key={headerGroup.id}>
+                    {headerGroup.headers.map((header) => {
+                      return (
+                        <TableHead key={header.id} className="text-center">
+                          {header.isPlaceholder
+                            ? null
+                            : flexRender(header.column.columnDef.header, header.getContext())}
+                        </TableHead>
+                      );
+                    })}
                   </TableRow>
-                ))
-              ) : (
-                <TableRow>
-                  <TableCell colSpan={columns.length} className="h-24 text-center">
-                    <div className="space-y-4">
-                      {loading
-                        ? [...Array(4)].map((_, index) => (
-                            <div key={index} className="flex items-center gap-4 p-2 border-b last:border-none">
-                              <Skeleton className="h-8 w-8 rounded-full" />
-                              <Skeleton className="h-8 w-full" />
-                            </div>
-                          ))
-                        : "No results"}
-                    </div>
-                  </TableCell>
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <div className="flex items-center justify-end space-x-2 py-4">
-          <div className="flex-1 text-sm text-muted-foreground">
-            {table.getFilteredSelectedRowModel().rows.length} of {table.getFilteredRowModel().rows.length} row(s)
-            selected.
-          </div>
-          <div className="space-x-2">
-            <Button
-              variant="outline"
-              size="sm"
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-            >
-              Previous
-            </Button>
-            <Button variant="outline" size="sm" onClick={() => table.nextPage()} disabled={!table.getCanNextPage()}>
-              Next
-            </Button>
-          </div>
+                ))}
+              </TableHeader>
+              <TableBody>
+                {table.getRowModel().rows?.length ? (
+                  table.getRowModel().rows.map((row) => (
+                    <TableRow key={row.id} data-state={row.getIsSelected() && "selected"}>
+                      {row.getVisibleCells().map((cell) => (
+                        <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>
+                      ))}
+                    </TableRow>
+                  ))
+                ) : (
+                  <TableRow>
+                    <TableCell colSpan={columns.length} className="h-24 text-center">
+                      <div className="space-y-4">
+                        {loading
+                          ? [...Array(4)].map((_, index) => (
+                              <div key={index} className="flex items-center gap-4 p-2 border-b last:border-none">
+                                <Skeleton className="h-8 w-8 rounded-full" />
+                                <Skeleton className="h-8 w-full" />
+                              </div>
+                            ))
+                          : "No results"}
+                      </div>
+                    </TableCell>
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </ScrollArea>
         </div>
       </div>
     </div>
