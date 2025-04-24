54a2a92bc018893f988fecfa660c81fd6776794c
lucssmassuh <lucas.massuh@gmail.com>
2025-04-23 21:51:51 -0300

    Updating searchbox hint

--- app/(protected)/automations/_components/addbot-button.tsx
+++ app/(protected)/automations/_components/addbot-button.tsx
@@ -80,7 +80,7 @@ function AddBotsButton({ buttonLabel = "Add", className = "" }: AddBotsButtonPro
                   <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
                   <Input
                     type="search"
-                    placeholder="Search products..."
+                    placeholder="Type to search inline..."
                     className="w-full appearance-none bg-background pl-8 shadow-none md:w-2/3 lg:w-1/3"
                     onChange={(event) => setSearch(event.target.value)}
                   />
