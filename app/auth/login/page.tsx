f614b54b4e4fd9da4601c81d6835c99ab55adf0e
lucssmassuh <lucas.massuh@gmail.com>
2025-05-11 00:48:49 -0300

    adding image to login screen

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -3,10 +3,10 @@ import { LoginForm } from "@/components/auth/login-form";
 const LoginPage = () => {
   return (
     <div style={{ minHeight: '100vh' }} className="flex w-full h-screen">
-      <div className="flex items-center justify-center w-full md:w-[30%] bg-white z-10">
+      <div className="flex items-center justify-center w-full md:w-[40%] bg-white z-10">
         <LoginForm />
       </div>
-      <div className="hidden md:block w-[70%] h-full relative">
+      <div className="hidden md:block w-[60%] h-full relative">
         <img
           src="/cald-starry-night.jpg"
           alt="Starry Night"
