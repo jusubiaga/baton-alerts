25706b0c6238a026f98325fae8ec5437c74e433e
lucssmassuh <lucas.massuh@gmail.com>
2025-05-11 00:27:19 -0300

    feat(login): enhance login screen with image layout

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,9 +1,20 @@
 import { LoginForm } from "@/components/auth/login-form";
 
 const LoginPage = () => {
-  return ( 
-    <LoginForm />
+  return (
+    <div style={{ minHeight: '100vh' }} className="flex w-full h-screen">
+      <div className="flex items-center justify-center w-full md:w-[30%] bg-white z-10">
+        <LoginForm />
+      </div>
+      <div className="hidden md:block w-[70%] h-full relative">
+        <img
+          src="/cald-starry-night.jpg"
+          alt="Starry Night"
+          className="object-cover w-full h-full"
+        />
+      </div>
+    </div>
   );
-}
- 
+};
+
 export default LoginPage;
