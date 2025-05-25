1f9336f879eece5b4c610a26178079accee4dc89
Test User <test@example.com>
2025-05-25 18:04:55 -0300

    feat: add workspace

--- app/(protected)/(workspace)/workspace/loading.tsx
+++ app/(protected)/(workspace)/workspace/loading.tsx
@@ -1,6 +1,3 @@
-import { Skeleton } from "@/components/ui/skeleton";
-import Loading from "../../components/loading";
-
 function loading() {
   return <div>Loading ...</div>;
 }
--- app/(protected)/(workspace)/workspace/page.tsx
+++ app/(protected)/(workspace)/workspace/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { use, useEffect } from "react";
 import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
@@ -19,12 +19,25 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useCurrentUser } from "@/hooks/use-current-user";
+import { User } from "next-auth";
+
+import {
+  createWorkspaceAction,
+  deleteWorkspaceAction,
+  getWorkspaceAction,
+  updateWorkspaceAction,
+} from "@/actions/workspace";
+import { fi } from "date-fns/locale";
+import { toast } from "sonner";
+import { CardSkeleton } from "./_components/CardSkeleton";
 
 interface Workspace {
-  id?: number;
+  id?: string;
   name: string;
   description: string;
   icon: string;
+  user: string;
   workspaceMembers: string;
   integrationsCount: number;
   botsCount: number;
@@ -36,63 +49,29 @@ const WorkspacePage = () => {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [currentWorkspace, setCurrentWorkspace] = useState<Workspace | null>(null);
   const [workspaces, setWorkspaces] = useState<Workspace[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const user = useCurrentUser();
 
-  // const workspaceData: Workspace[] = [];
-  // [
-  //   {
-  //     id: 1,
-  //     name: "Taxfix - Prod",
-  //     description:
-  //       "Taxfix Production Account, don't make changes without verifying with the Paid Acquisition team or Maestro's PM. Talk to Evgenii for access.",
-  //     icon: "/images/taxfix.png",
-  //     workspaceMembers: "jus@gmail.com",
-  //     integrationsCount: 8,
-  //     botsCount: 14,
-  //   },
-  //   {
-  //     id: 2,
-  //     name: "Taxfix - Sandbox",
-  //     description: "Taxfix Sandbox Account, feel free to mess around. Integrations get deleted every week.",
-  //     icon: "/images/taxfix.png",
-  //     workspaceMembers: "jus@gmail.com",
-  //     integrationsCount: 18,
-  //     botsCount: 140,
-  //   },
-  //   {
-  //     id: 3,
-  //     name: "Steuerbot - Prod",
-  //     description: "Steuerbot production workspace, talk to Marc L for access.",
-  //     icon: "/images/steuerbot.png",
-  //     workspaceMembers: "jus@gmail.com",
-  //     integrationsCount: 8,
-  //     botsCount: 14,
-  //   },
-  //   {
-  //     id: 4,
-  //     name: "TaxScouts - UK",
-  //     description: "TS Prod, talk to Oli for access.",
-  //     icon: "/images/taxscouts.png",
-  //     workspaceMembers: "jus@gmail.com",
-  //     integrationsCount: 8,
-  //     botsCount: 14,
-  //   },
-  //   {
-  //     id: 5,
-  //     name: "Taxscouts - ES",
-  //     description: "TS Prod Spain, talk to Miguel for access.",
-  //     icon: "/images/taxscouts.png",
-  //     workspaceMembers: "jus@gmail.com",
-  //     integrationsCount: 8,
-  //     botsCount: 14,
-  //   },
-  // ];
+  const getWorkSpaces = async () => {
+    console.log("getWorkSpaces called"); // Log para ver si se llama
+    setIsLoading(true);
+    const workspaces = await getWorkspaceAction();
+    setWorkspaces(workspaces);
+    setIsLoading(false);
+    console.log("Workspaces fetched:", workspaces); // Log para ver los datos iniciales
+  };
+
+  useEffect(() => {
+    getWorkSpaces();
+  }, []);
 
   const handleNewWorkspaceClick = () => {
     setCurrentWorkspace({
       name: "",
       description: "",
       icon: "",
-      workspaceMembers: "",
+      user: user?.id ?? "",
+      workspaceMembers: user?.email ?? "",
       integrationsCount: 0,
       botsCount: 0,
     });
@@ -108,19 +87,73 @@ const WorkspacePage = () => {
     router.push("/runlog");
   };
 
-  const handleSaveWorkspace = () => {
-    // console.log("handleSaveWorkspace", event);
-    setWorkspaces((workspaces) => [...workspaces, currentWorkspace!]);
-    // setWorkspaces((workspaces) => [...workspaces, event as Workspace]);
-    // TODO: Implement save logic (create new or update existing)
+  const handleSaveWorkspace = async () => {
     console.log("Saving workspace:", currentWorkspace);
     setIsDialogOpen(false);
+
+    try {
+      setIsLoading(true);
+      await createWorkspaceAction(currentWorkspace);
+
+      console.log("createWorkspaceAction successful");
+      await getWorkSpaces();
+
+      console.log("router.refresh() called after save");
+      toast.success("Workspace created successfully!");
+    } catch (error) {
+      console.error("Error creating workspace:", error);
+      toast.error("Failed to create workspace.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleUpdateWorkspace = async () => {
+    if (currentWorkspace?.id) {
+      console.log("Updating workspace:", currentWorkspace);
+      setIsDialogOpen(false);
+
+      try {
+        setIsLoading(true);
+        await updateWorkspaceAction(currentWorkspace?.id, currentWorkspace);
+
+        console.log("updateWorkspaceAction successful"); // Log de éxito
+        await getWorkSpaces();
+        console.log("router.refresh() called after update");
+        toast.success("Workspace updated successfully!");
+      } catch (error) {
+        console.error("Error updating workspace:", error);
+        toast.error("Failed to update workspace.");
+      } finally {
+        setIsLoading(false);
+      }
+    } else {
+      // Manejar el caso en que no hay ID para actualizar (aunque el botón debería estar deshabilitado/oculto en este caso)
+      console.warn("Attempted to update workspace without an ID.");
+      // toast.warning("Cannot update workspace without an ID.");
+    }
+  };
+
+  const handleDeleteClick = async (id: string) => {
+    if (id) {
+      try {
+        setIsLoading(true);
+        await deleteWorkspaceAction(id);
+        await getWorkSpaces();
+        toast.success("Workspace deleted successfully!");
+      } catch (error) {
+        console.error("Error deleting workspace:", error);
+        toast.error("Failed to delete workspace.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
   };
 
   return (
     <div className="flex flex-col items-center w-full p-4">
       <div className="flex flex-col items-center gap-2 mb-8 text-center">
-        <h3 className="text-3xl font-semibold">Welcome, Lucas!</h3>
+        <h3 className="text-3xl font-semibold">Welcome, {user?.name}</h3>
         <p className="text-sm text-muted-foreground">
           {workspaces.length === 0
             ? "Let’s create your first workspace"
@@ -130,42 +163,58 @@ const WorkspacePage = () => {
           New Workspace
         </Button>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-5xl">
-        {workspaces.map((workspace) => (
-          <Card key={workspace.id} className="flex flex-col">
-            <CardHeader className="flex flex-row items-center gap-4 pb-2">
-              <Avatar>
-                <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
-                <AvatarFallback>WS</AvatarFallback>
-              </Avatar>
-              <div className="flex flex-col">
-                <CardTitle className="text-lg">{workspace.name}</CardTitle>
-              </div>
-            </CardHeader>
-            <Separator />
-            <CardContent className="flex-grow py-4 text-sm text-muted-foreground">{workspace.description}</CardContent>
-            <Separator />
-            <CardContent className="py-4">
-              <p className="text-xs text-muted-foreground">{workspace.integrationsCount} Integrations</p>
-              <p className="text-xs text-muted-foreground">{workspace.botsCount} Bots configured</p>
-            </CardContent>
-            <CardFooter className="flex justify-end gap-2 pt-2">
-              <Button variant="outline" size="sm" onClick={() => handleSetupClick(workspace)}>
-                Setup
-              </Button>
-              <Button size="sm" onClick={handleOpenClick}>
-                Open
-              </Button>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+
+      {isLoading ? (
+        <div className="flex gap-2 flex-wrap items-center justify-center">
+          <CardSkeleton></CardSkeleton>
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-5xl">
+          {workspaces.map((workspace) => (
+            <Card key={workspace.id} className="flex flex-col">
+              <CardHeader className="flex flex-row items-center gap-4 pb-2">
+                <Avatar>
+                  <AvatarImage src="https://github.com/shadcn.png" alt="@shadcn" />
+                  <AvatarFallback>WS</AvatarFallback>
+                </Avatar>
+                <div className="flex flex-col">
+                  <CardTitle className="text-lg">{workspace.name}</CardTitle>
+                </div>
+              </CardHeader>
+              <Separator />
+              <CardContent className="flex-grow py-4 text-sm text-muted-foreground">
+                {workspace.description}
+              </CardContent>
+              <Separator />
+              <CardContent className="py-4">
+                <p className="text-xs text-muted-foreground">{workspace.integrationsCount} Integrations</p>
+                <p className="text-xs text-muted-foreground">{workspace.botsCount} Bots configured</p>
+              </CardContent>
+              <CardFooter className="flex justify-end gap-2 pt-2">
+                {/* {user?.id === workspace.user && (
+                  <Button variant="destructive" size="sm" onClick={() => handleDeleteClick(workspace?.id ?? "")}>
+                    Delete
+                  </Button>
+                )}
+ */}
+                <Button variant="outline" size="sm" onClick={() => handleSetupClick(workspace)}>
+                  Setup
+                </Button>
+                <Button size="sm" onClick={handleOpenClick}>
+                  Open
+                </Button>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
+
       {/* Workspace Dialog */}
       <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
         <DialogContent className="sm:max-w-[425px]">
           <DialogHeader>
             <DialogTitle>
-              {currentWorkspace?.id ? `Edit Workspace: ${currentWorkspace.name}` : "Create New Workspace"}{" "}
+              {currentWorkspace?.id ? `Edit Workspace: ${currentWorkspace.name}` : "Create New Workspace"}
               {/* Check for id to determine edit/create */}
             </DialogTitle>
             <DialogDescription>
@@ -222,7 +271,7 @@ const WorkspacePage = () => {
                 value={currentWorkspace?.workspaceMembers || ""}
                 onChange={(e) => {
                   if (currentWorkspace) {
-                    setCurrentWorkspace({ ...currentWorkspace, workspaceMembers: e.target.value });
+                    setCurrentWorkspace({ ...currentWorkspace, workspaceMembers: e?.target?.value });
                   }
                 }}
                 className="col-span-3"
@@ -230,9 +279,15 @@ const WorkspacePage = () => {
             </div>
           </div>
           <DialogFooter>
-            <Button type="submit" onClick={handleSaveWorkspace}>
-              Save changes
-            </Button>
+            {currentWorkspace?.id ? (
+              <Button type="submit" onClick={handleUpdateWorkspace}>
+                Update changes
+              </Button>
+            ) : (
+              <Button type="submit" onClick={handleSaveWorkspace}>
+                Save changes
+              </Button>
+            )}
           </DialogFooter>
         </DialogContent>
       </Dialog>
